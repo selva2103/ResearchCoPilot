28e29610ff73ad25d713c1401e443adbea27acb6
Nadarj728 <61955185-nadarj728@users.noreply.replit.com>
2026-07-11 08:54:05 +0000

    WIP: preserve interrupted Phase 5.4C closure work

--- artifacts/research-copilot/.next/static/chunks/webpack.js
+++ artifacts/research-copilot/.next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("d806c9527538c199")
+/******/ 		__webpack_require__.h = () => ("8268064065d7976f")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
